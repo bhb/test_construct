666b0bae5240d446d87df4580f9ca2f06b1d685b
Ben Brinckerhoff <ben@bbrinck.com>
2013-11-26 19:19:03 -0800

    Add a dummy test. Run with 'rake test'

---
+++ spec/spec_helper.rb
@@ -0,0 +1,5 @@
+require 'minitest/spec'
+require 'minitest/autorun'
+require 'minitest/pride'
+
+
---
+++ spec/test_construct_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+
+describe "TestConstruct" do
+
+  it "test" do
+  end
+
+end
