fedeb9ba0e516861d91040a550a249f820a357b6
Ben Brinckerhoff <ben@bbrinck.com>
2015-02-14 16:42:02 -0800

    Shortens rspec integration test and changes assertion to use test_construct

--- test/rspec_integration_test.rb
+++ test/rspec_integration_test.rb
@@ -1,36 +1,24 @@
 require 'test_helper'
 
-def unindent(s)
-  s.gsub(/^#{s.scan(/^[ \t]+(?=\S)/).min}/, '')
-end
-
-# Test harness for test_construct rspec integration
-#
 class RspecIntegrationTest < Minitest::Test
   include TestConstruct::Helpers
 
-  def teardown
-    Dir.chdir File.expand_path('../..', __FILE__)
-    TestConstruct.destroy_all!
+  test 'rspec integration' do
+    lib_path = File.realpath('lib')
+    within_construct do |construct|
+      spec_file_name = 'rspec_spec.rb'
+      construct.file(spec_file_name, <<-RSPEC)
+require 'test_construct/rspec_integration'
+
+describe 'test_construct', test_construct: true do
+  it 'accesses metadata' do |example|
+    f = example.metadata[:construct].file "somefile", "abcd"
+    expect(f.size).to eq 4
   end
-
-  testing 'using the test_construct' do
-    test 'using test_construct does not cause an error' do
-      lib_path = File.realpath('lib')
-      within_construct do |construct|
-        spec_file_name = 'test_construct_spec.rb'
-        construct.file(spec_file_name, unindent(<<-RSPEC))
-          require 'test_construct/rspec_integration'
-
-          describe 'test_construct', test_construct: true do
-            it 'should execute a test that always passes' do
-              expect(1).to eq(1)
-            end
-          end
-        RSPEC
-        output = `rspec -I '#{lib_path}' #{spec_file_name}`
-        assert $CHILD_STATUS.success?, output
-      end
+end
+      RSPEC
+      output = `rspec -I '#{lib_path}' #{spec_file_name}`
+      assert $CHILD_STATUS.success?, output
     end
   end
 end
--- test_construct.gemspec
+++ test_construct.gemspec
@@ -22,6 +22,5 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "rake"
   spec.add_development_dependency "minitest", "~> 5.0.8"
   spec.add_development_dependency "mocha", "~> 0.14.0"
-  spec.add_development_dependency "debugger"
-  spec.add_development_dependency "rspec"
+  spec.add_development_dependency "rspec", "~> 3.0"
 end
